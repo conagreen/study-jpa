5273e3f5b89eb733bdb32915ba46ce7ab06229e1
conagreen <icoqwee@gmail.com>
2022-01-19 01:02:28 +0900

    EntityManager로 객체 crud 연습

---
+++ jpa-basic/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,37 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        final EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        final EntityManager em = emf.createEntityManager();
+
+        final EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+//            final Member findMember = em.find(Member.class, 2L);
+            final List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .setFirstResult(5)
+                    .setMaxResults(8)
+                    .getResultList();
+
+            for (Member member : result) {
+                System.out.println("member.name = " + member.getName());
+            }
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
